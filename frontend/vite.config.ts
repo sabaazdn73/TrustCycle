97c4dafdd19be0e6a257d043b31071898314e531
Saba Azadegan <sabaazadegan@Mac.lan>
2026-02-23 14:28:29 +0000

    Optimize bundle size with manualChunks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Load faster
+          if (id.includes('@iota')) {
+            return 'iota-sdk';
+          }
+          if (id.includes('react-globe.gl') || id.includes('three')) {
+            return 'globe-visuals';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
