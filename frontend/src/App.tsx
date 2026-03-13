94ca8ea4289040acd844a4cd7d1cb2e74e675342
Saba Azadegan <sabaazadegan@Mac.lan>
2026-03-13 14:23:18 +0000

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -980,6 +980,9 @@ export default function App() {
           {' .'}
           <TechnicalAssistant />
         </p>
+        <p style={{ fontSize: '10px', color: '#817d7de9', margin: '6px 0 0', fontFamily: 'monospace' }}>
+          <a href="https://github.com/sabaazdn73/TrustCycle-DRS" target="_blank" style={{ fontWeight: 'bold', color: '#8c8a8ac7', textDecoration: 'none', borderBottom: '1px dotted #7e7d7d' }}> Demo Walkthrough </a>
+          </p>
       </div>
     </div>
   );
