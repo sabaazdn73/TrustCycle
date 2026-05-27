d02ebd58b757e02a3055fbd6acfe48fe690545e9
Saba Azadegan <sabaazadegan@Mac.lan>
2026-05-27 16:34:52 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1036,6 +1036,11 @@ export default function App() {
           <a href="https://youtu.be/OXa0zkTi3Vs" target="_blank" style={{ fontWeight: 'bold', color: '#8c8a8ac7', textDecoration: 'none', borderBottom: '1px dotted #7e7d7d' }}> Demo Walkthrough </a>
           </p>
 
+        <p style={{ fontSize: '11px', color: '#817d7de9', margin: '6px 0 0', fontFamily: 'monospace', textAlign: 'center'}}>
+          FEEDBACK/HELP?{' '}
+          <a href="saba@trustcycle.tech" target="_blank" style={{ fontWeight: 'bold', color: '#8c8a8ac7', textDecoration: 'none', borderBottom: '1px dotted #7e7d7d' }}> Contact Me </a>
+          </p>
+
       </div>
       </>
       )}
